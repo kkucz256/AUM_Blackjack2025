b3b860df0db9e063ffdf9a56051f80e8d6ddf24c
Krzysztof <krzysiek@MacBook-Air-Krzysiek.local>
2025-03-16 11:02:38 +0100

    fixed card popping and added hand value

--- main.py
+++ main.py
@@ -6,14 +6,37 @@ from utils import dealCardToPlayer, dealCardToDealer
 def main():
     deck1 = Deck()
     deck1.shuffleDeck()
-    
+
     player = Player()
-    print(deck1.deck)
+    dealer = Player()
+    print(f"Initial deck:\n{deck1.deck}\n")
     playerHand, newDeck=dealCardToPlayer(player.hand, deck1)
-    print(playerHand)
-    print(newDeck.deck)
+    dealerHand, newDeck=dealCardToDealer(dealer.hand, newDeck)
+    playerHand, newDeck=dealCardToPlayer(player.hand, newDeck)
     
-    # dealCardToDealer()
+    print(f"Deck after initial dealing:\n{newDeck.deck}\n")
+    print(f"Player's hand:\n{playerHand}\n")
+    print(f"Dealer's hand:\n{dealerHand}")
+    
+        
+    # player = Player()
+    # player.hand = [('ACE', (1, 11)), ('KING', 10)]
+    # print(player.totalHandValue())
+
+    # player.hand = [('ACE', (1, 11)), ('NINE', 9)]
+    # print(player.totalHandValue())
+
+    # player.hand = [('ACE', (1, 11)), ('NINE', 9), ('TWO', 2)]
+    # print(player.totalHandValue())
+
+    # player.hand = [('ACE', (1, 11)), ('ACE', (1, 11)), ('NINE', 9)]
+    # print(player.totalHandValue())
+    
+    # player.hand = [('ACE', (1, 11)) for i in range(0,11)]
+    # print(player.totalHandValue())
+    
+    
+
  
 if __name__ == "__main__":
     main()
--- utils/functions.py
+++ utils/functions.py
@@ -3,10 +3,10 @@
 
 def dealCardToPlayer(playerCards, deck):
     playerCards.append(deck.deck[0])
-    deck.deck.pop()
+    deck.deck.pop(0)
     return playerCards, deck
 
 def dealCardToDealer(dealerCards, deck):
     dealerCards.append(deck.deck[0])
-    deck.deck.pop()
+    deck.deck.pop(0)
     return dealerCards, deck
